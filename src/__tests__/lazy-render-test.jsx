a7b20ffab22fa1c7621f44bb354bec48d07e5606
Jonathan Kim <jkimbo@gmail.com>
2015-03-01 12:31:26 +0000

    Added tests for padding

--- src/__tests__/lazy-render-test.jsx
+++ src/__tests__/lazy-render-test.jsx
@@ -25,5 +25,41 @@ describe('LazyRender', function() {
     );
     expect(renderedChildren.length).toEqual(10);
   });
+
+  it('only renders children that are visible', function() {
+    var children = [];
+    for (var i = 0; i < 100; i++) {
+      children.push(<div className="child" style={{ height: 20 }}>{i}</div>);
+    }
+
+    var lazy = TestUtils.renderIntoDocument(
+      <LazyRender maxHeight={200}>
+        {children}
+      </LazyRender>
+    );
+
+    var renderedChildren = TestUtils.scryRenderedDOMComponentsWithClass(
+      lazy, 'child'
+    );
+    expect(renderedChildren.length).toEqual(13); // item of children plus default padding
+  });
+
+  it('renders configurable number of children for padding', function() {
+    var children = [];
+    for (var i = 0; i < 100; i++) {
+      children.push(<div className="child" style={{ height: 20 }}>{i}</div>);
+    }
+
+    var lazy = TestUtils.renderIntoDocument(
+      <LazyRender maxHeight={200} itemPadding={5}>
+        {children}
+      </LazyRender>
+    );
+
+    var renderedChildren = TestUtils.scryRenderedDOMComponentsWithClass(
+      lazy, 'child'
+    );
+    expect(renderedChildren.length).toEqual(15);
+  });
 });
 
