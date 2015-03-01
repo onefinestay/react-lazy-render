5a2792b5d614eaebb0a0efb2a6dce0fb1a12693a
Jonathan Kim <jkimbo@gmail.com>
2015-03-01 12:10:38 +0000

    Make item padding configurable. Fixes #2

--- src/lazy-render.jsx
+++ src/lazy-render.jsx
@@ -8,12 +8,14 @@ var cx = React.addons.classSet;
 var LazyRender = React.createClass({
   propTypes: {
     children: React.PropTypes.array.isRequired,
-    maxHeight: React.PropTypes.number.isRequired
+    maxHeight: React.PropTypes.number.isRequired,
+
+    itemPadding: React.PropTypes.number
   },
 
   getDefaultProps: function() {
     return {
-      padding: 3
+      itemPadding: 3
     };
   },
 
@@ -72,7 +74,7 @@ var LazyRender = React.createClass({
     var numberOfItems = Math.ceil(height / this.state.childHeight);
 
     if (height === this.props.maxHeight) {
-      numberOfItems += this.props.padding;
+      numberOfItems += this.props.itemPadding;
     }
 
     this.setState({
@@ -96,7 +98,7 @@ var LazyRender = React.createClass({
     var numberOfItems = Math.ceil(height / childHeight);
 
     if (height === this.props.maxHeight) {
-      numberOfItems += this.props.padding;
+      numberOfItems += this.props.itemPadding;
     }
 
     this.setState({
