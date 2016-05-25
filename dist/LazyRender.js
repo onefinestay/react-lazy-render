5c8fcc64665979950bcb983ce458fa6135385a51
Stephen Smith <stephensmith@Stephens-MacBook-Pro.local>
2016-05-25 17:07:04 -0400

    bugfix
    * Fixed jitter bug
    * build now works with npm build
    * Fixed initial js error

--- dist/LazyRender.js
+++ dist/LazyRender.js
@@ -1,6 +1,7 @@
 "use strict";
 
 var React = require('react');
+var ReactDOM = require('react-dom');
 var elementSize = require("element-size");
 
 var LazyRender = React.createClass({displayName: "LazyRender",
@@ -49,7 +50,16 @@ var LazyRender = React.createClass({displayName: "LazyRender",
     var scrollTop = container.scrollTop;
     var childrenLength = this.getChildrenLength(this.props);
 
-    var childrenTop = Math.floor(scrollTop / this.state.childHeight);
+    var oldChildTop = this.state.childrenTop;
+    var calcTop = scrollTop / this.state.childHeight;
+    var tolerance = 0.1;
+
+    var childrenTop = oldChildTop
+
+    if(Math.abs(oldChildTop - calcTop) > tolerance) {
+      childrenTop = Math.floor(calcTop);
+    }
+
     var childrenBottom = (childrenLength - childrenTop -
                           this.state.childrenToRender);
 
@@ -181,7 +191,7 @@ var LazyRender = React.createClass({displayName: "LazyRender",
   },
 
   getChildHeight: function() {
-    var firstChild = this.refs['child-0'];
+    var firstChild = ReactDOM.findDOMNode(this.refs['child-0']);
 
     return this.getElementHeight(firstChild);
   },
